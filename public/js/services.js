efd0345712a8e14a73b4e56fda779a7426f2d0d4
rokas <rokasra@gmail.com>
2022-04-22 10:12:00 +0300

    render card

--- public/js/services.js
+++ public/js/services.js
@@ -1,19 +1,36 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line import/extensions
+import { createCard } from './modules/html.js';
+
 const BASE_URL = 'http://localhost:3100/api';
-console.log('services.js');
+console.log('sevices.js');
+
+const cardContainerEl = document.querySelector('.cards-container');
 
 async function getServices() {
   try {
     const resp = await fetch(`${BASE_URL}/services`);
-    console.log('resp===', resp);
+    console.log('resp ===', resp);
     // fetch nepermeta kodo i catch bloka jei status yra klaidos
     if (resp.ok === false) throw new Error('something is wrong');
 
     const servicesArr = await resp.json();
-    console.log('servicesArr===', servicesArr);
+    console.log('servicesArr ===', servicesArr);
     console.log('piesiam korteles');
+    renderCards(servicesArr, cardContainerEl);
+    // createCard(servicesArr[0]);
   } catch (error) {
-    console.log('error ===', error);
+    console.warn('error ===', error);
     console.log('atvaizduojam klaida');
   }
 }
+
+function renderCards(cardArr, dest) {
+  // isvalyti dest kad neliktu pries tai buvusiu korteliu
+  // sukti cikla ir irasyti visas gautas korteles
+  const card = createCard(cardArr[0]);
+  dest.append(card);
+}
+//
+
 getServices();
