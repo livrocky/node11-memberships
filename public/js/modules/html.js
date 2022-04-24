3c5a40be30baca69099fa65b8f7bb9fa2a00dc2f
rokas <rokasra@gmail.com>
2022-04-24 20:37:59 +0300

    services, html js

--- public/js/modules/html.js
+++ public/js/modules/html.js
@@ -1,22 +1,38 @@
-/* <div class="membership-card">
-<h2 class="membership-title">$1.99 Trial</h2>
-<p class="membership-desc">This is 2 days membership</p>
-<hr>
-<div class="trash-icon">
-    <i class="fa fa-trash" aria-hidden="true"></i>
-</div>
-</div> */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-cycle */
+import { deleteService } from '../services.js';
 
-function createCard() {
-  const divEl = document.createElement('div');
-  makeEl('h2', 'Price Title', divEl);
-  makeEl('p', 'Price Title', divEl);
-  makeEl('h2', 'Price Title', divEl);
-}
-
-function makeEl(tagName, text, dest, eClass = null) {
+function makeEl(tagName, text, dest, elClass = null) {
   const el = document.createElement(tagName);
   el.textContent = text;
-  if (eClass) el.className = eClass;
-  dest.
+  if (elClass) el.className = elClass;
+  dest.appendChild(el);
+  return el;
+}
+
+export function createCard(newCardObj) {
+  const articleEl = document.createElement('article');
+  articleEl.className = 'membership-card';
+  makeEl('h3', `${newCardObj.price}eur ${newCardObj.name}`, articleEl);
+  makeEl('p', newCardObj.description, articleEl);
+  makeEl('div', '', articleEl, 'hr');
+  // makeEl('button', 'delete', articleEl, 'btn btn-delete');
+  const btn = makeEl('button', '', articleEl, 'btn btn-delete');
+  makeEl('i', '', btn, 'fa fa-trash');
+  btn.onclick = () => {
+    console.log('delete', newCardObj.id);
+    deleteService(newCardObj.id);
+  };
+
+  return articleEl;
+}
+
+export function renderCards(cardArr, dest) {
+  // isvalyti dest kad neliktu pries tai buvusiu korteliu
+  // dest.innerHTML = '';
+  // sukti cikla ir irasyti visas gautas korteles
+  cardArr.forEach((cObj) => {
+    const card = createCard(cObj);
+    dest.append(card);
+  });
 }
--- public/js/services.js
+++ public/js/services.js
@@ -1,5 +1,13 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+import { renderCards } from './modules/html.js';
+
 const BASE_URL = 'http://localhost:3100/api';
 console.log('services.js');
+
+const cardContainerEl = document.querySelector('.cards-display');
+// const addBntEl = document.querySelector('.membership-btn');
 
 // GET
 
@@ -11,11 +19,28 @@ async function getServices() {
     if (resp.ok === false) throw new Error('something is wrong');
 
     const servicesArr = await resp.json();
-    console.log('servicesArr===', servicesArr);
+    console.log('servicesArr ===', servicesArr);
     console.log('piesiam korteles');
+    renderCards(servicesArr, cardContainerEl);
+    // createCard(servicesArr[0]);
   } catch (error) {
-    console.log('error ===', error);
+    console.warn('error ===', error);
     console.log('atvaizduojam klaida');
   }
 }
+
+export async function deleteService(idToDelete) {
+  try {
+    const resp = await fetch(`${BASE_URL}/services/${idToDelete}`, { method: 'DELETE' });
+    console.log('resp ===', resp);
+    // fetch nepermeta kodo i catch bloka jei status yra klaidos
+    if (resp.ok === false) throw new Error('error deleting');
+    getServices();
+    // createCard(servicesArr[0]);
+  } catch (error) {
+    console.warn('error ===', error);
+    console.log('atvaizduojam klaida');
+  }
+}
+
 getServices();
